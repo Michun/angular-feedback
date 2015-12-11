d1bf3f6375a6345a47d77b75f7d6094d3aeafb09
Michal Nowacki <mnowacki@neoteric.eu>
2015-12-11 14:33:28 +0100

    some styling

--- dist/angular-send-feedback.js
+++ dist/angular-send-feedback.js
@@ -7,8 +7,8 @@
 angular.module('templates-angularsendfeedback', ['angularsendfeedback.html']);
 
 angular.module("angularsendfeedback.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("angularsendfeedback.html",
-    "");
+	$templateCache.put("angularsendfeedback.html",
+		"");
 }]);
 
 angular.module('angular-send-feedback', ['templates-angularsendfeedback']);
@@ -72,7 +72,7 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', '
 				tpl: {
 					description: '<div id="feedback-welcome"><div class="feedback-logo">Feedback</div><p>Feedback lets you send us suggestions about our products. We welcome problem reports, feature ideas and general comments.</p><p>Start by writing a brief description:</p><textarea id="feedback-note-tmp"></textarea><p>Next we\'ll let you identify areas of the page related to your description.</p><button id="feedback-welcome-next" class="feedback-next-btn feedback-btn-gray">Next</button><div id="feedback-welcome-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
 					highlighter: '<div id="feedback-highlighter"><div class="feedback-logo">Feedback</div><p>Click and drag on the page to help us better understand your feedback. You can move this dialog if it\'s in the way.</p><button class="feedback-sethighlight feedback-active"><div class="ico"></div><span>Highlight</span></button><label>Highlight areas relevant to your feedback.</label><button class="feedback-setblackout"><div class="ico"></div><span>Black out</span></button><label class="lower">Black out any personal information.</label><div class="feedback-buttons"><button id="feedback-highlighter-next" class="feedback-next-btn feedback-btn-gray">Next</button><button id="feedback-highlighter-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div class="feedback-wizard-close"></div></div>',
-					overview: '<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Subject</h3><h3>Description</h3><h3 class="feedback-additional">Additional info</h3><div id="feedback-additional-none"><span>None</span></div><div id="feedback-browser-info"><span>Browser Info</span></div><div id="feedback-page-info"><span>Page Info</span></div><div id="feedback-page-structure"><span>Page Structure</span></div></div></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
+					overview: '<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Subject</h3><h3>Description</h3></div></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
 					submitSuccess: '<div id="feedback-submit-success"><div class="feedback-logo">Feedback</div><p>Thank you for your feedback. We value every piece of feedback we receive.</p><p>We cannot respond individually to every one, but we will use your comments as we strive to improve your experience.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>',
 					submitError: '<div id="feedback-submit-error"><div class="feedback-logo">Feedback</div><p>Sadly an error occured while sending your feedback. Please try again.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>'
 				},
@@ -515,8 +515,8 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', '
 									$('#feedback-overview').show();
 									$('#feedback-overview-description-text>textarea').remove();
 									$('#feedback-overview-screenshot>img').remove();
-									$('<textarea id="feedback-overview-subject">' + ' </textarea>').insertAfter('#feedback-overview-description-text h3:eq(0)');
-									$('<textarea id="feedback-overview-note">' + $('#feedback-note').val() + '</textarea>').insertAfter('#feedback-overview-description-text h3:eq(1)');
+									$('<textarea placeholder="Subject of feedback" id="feedback-overview-subject">' + '</textarea>').insertAfter('#feedback-overview-description-text h3:eq(0)');
+									$('<textarea placeholder="Describe your problem in detail" id="feedback-overview-note">' + $('#feedback-note').val() + '</textarea>').insertAfter('#feedback-overview-description-text h3:eq(1)');
 									$('#feedback-overview-screenshot').append('<img class="feedback-screenshot" src="' + img + '" />');
 								}
 								else {
