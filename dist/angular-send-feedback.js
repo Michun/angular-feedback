6b775bd12c4264f3589eb978ec2d0e8b9edd42f8
Michał Nowacki <mnowacki@neoteric.eu>
2015-12-11 01:42:20 +0100

    subject, user, and some more added

--- dist/angular-send-feedback.js
+++ dist/angular-send-feedback.js
@@ -13,7 +13,7 @@ angular.module("angularsendfeedback.html", []).run(["$templateCache", function($
 
 angular.module('angular-send-feedback', ['templates-angularsendfeedback']);
 
-angular.module('angular-send-feedback').directive('angularFeedback', ['$http', function ($http) {
+angular.module('angular-send-feedback').directive('angularFeedback', ['$http', '$rootScope', '$location', function ($http, $rootScope, $location) {
 
 
     return {
@@ -24,6 +24,7 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', f
             options: '='
         },
         link: function ($scope) {
+
 
             var taigaUrl = $scope.options.taigaUrl;
             var taigaUser = $scope.options.taigaUser;
@@ -71,7 +72,7 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', f
                 tpl: {
                     description: '<div id="feedback-welcome"><div class="feedback-logo">Feedback</div><p>Feedback lets you send us suggestions about our products. We welcome problem reports, feature ideas and general comments.</p><p>Start by writing a brief description:</p><textarea id="feedback-note-tmp"></textarea><p>Next we\'ll let you identify areas of the page related to your description.</p><button id="feedback-welcome-next" class="feedback-next-btn feedback-btn-gray">Next</button><div id="feedback-welcome-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
                     highlighter: '<div id="feedback-highlighter"><div class="feedback-logo">Feedback</div><p>Click and drag on the page to help us better understand your feedback. You can move this dialog if it\'s in the way.</p><button class="feedback-sethighlight feedback-active"><div class="ico"></div><span>Highlight</span></button><label>Highlight areas relevant to your feedback.</label><button class="feedback-setblackout"><div class="ico"></div><span>Black out</span></button><label class="lower">Black out any personal information.</label><div class="feedback-buttons"><button id="feedback-highlighter-next" class="feedback-next-btn feedback-btn-gray">Next</button><button id="feedback-highlighter-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div class="feedback-wizard-close"></div></div>',
-                    overview: '<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Description</h3><h3 class="feedback-additional">Additional info</h3><div id="feedback-additional-none"><span>None</span></div><div id="feedback-browser-info"><span>Browser Info</span></div><div id="feedback-page-info"><span>Page Info</span></div><div id="feedback-page-structure"><span>Page Structure</span></div></div></div><div id="feedback-overview-screenshot"><h3>Screenshot</h3></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
+                    overview: '<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Subject</h3><h3>Description</h3><h3 class="feedback-additional">Additional info</h3><div id="feedback-additional-none"><span>None</span></div><div id="feedback-browser-info"><span>Browser Info</span></div><div id="feedback-page-info"><span>Page Info</span></div><div id="feedback-page-structure"><span>Page Structure</span></div></div></div><div id="feedback-overview-screenshot"><h3>Screenshot</h3></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
                     submitSuccess: '<div id="feedback-submit-success"><div class="feedback-logo">Feedback</div><p>Thank you for your feedback. We value every piece of feedback we receive.</p><p>We cannot respond individually to every one, but we will use your comments as we strive to improve your experience.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>',
                     submitError: '<div id="feedback-submit-error"><div class="feedback-logo">Feedback</div><p>Sadly an error occured while sending your feedback. Please try again.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>'
                 },
@@ -509,13 +510,13 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', f
                                 post.img = img;
                                 settings.onScreenshotTaken(post.img);
 
-                                console.log(img);
                                 if (settings.showDescriptionModal) {
                                     $('#feedback-canvas-tmp').remove();
                                     $('#feedback-overview').show();
                                     $('#feedback-overview-description-text>textarea').remove();
                                     $('#feedback-overview-screenshot>img').remove();
-                                    $('<textarea id="feedback-overview-note">' + $('#feedback-note').val() + '</textarea>').insertAfter('#feedback-overview-description-text h3:eq(0)');
+                                    $('<textarea id="feedback-overview-subject">' + ' </textarea>').insertAfter('#feedback-overview-description-text h3:eq(0)');
+                                    $('<textarea id="feedback-overview-note">' + $('#feedback-note').val() + '</textarea>').insertAfter('#feedback-overview-description-text h3:eq(1)');
                                     $('#feedback-overview-screenshot').append('<img class="feedback-screenshot" src="' + img + '" />');
                                 }
                                 else {
@@ -538,16 +539,20 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', f
                         $('#feedback-overview-error').hide();
                     });
 
-                    $(document).on('keyup', '#feedback-note-tmp,#feedback-overview-note', function (e) {
-                        var tx;
-                        if (e.target.id === 'feedback-note-tmp')
+                    $(document).on('keyup', '#feedback-note-tmp,#feedback-note-subject,#feedback-overview-note,#feedback-overview-subject', function (e) {
+                        var tx, sub;
+                        if (e.target.id === 'feedback-note-tmp'){
                             tx = $('#feedback-note-tmp').val();
-                        else {
+                            sub = $('#feedback-overview-subject').val();
+                        } else {
                             tx = $('#feedback-overview-note').val();
+                            sub = $('#feedback-overview-subject').val();
                             $('#feedback-note-tmp').val(tx);
+                            $('#feedback-subject-tmp').val(sub);
                         }
 
                         $('#feedback-note').val(tx);
+                        $('#feedback-subject').val(sub);
                     });
 
                     $(document).on('click', '#feedback-submit', function () {
@@ -594,6 +599,8 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', f
                             }
 
                             function submitIssue(request){
+                              request.description = request.description + ' ||| Informacje zalogowanym użytkowniku - id: ' + $rootScope.user.id + ', email: ' + $rootScope.user.email;
+                              request.description = request.description + ' ||| Adres Url: ' + $location.absUrl();
                                 $http({
                                     method: 'POST',
                                     url: taigaUrl + '/issues',
@@ -614,7 +621,7 @@ angular.module('angular-send-feedback').directive('angularFeedback', ['$http', f
 
                             submitIssue({
                                 project: taigaProject,
-                                subject: $('#feedback-note').val(),
+                                subject: $('#feedback-overview-subject').val(),
                                 description: $('#feedback-note').val()
                             });
                         }
